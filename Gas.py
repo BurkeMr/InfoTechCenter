38b4d300e2cd89e117781e0fc83c88971246f07b
Sean <Burkesean902@gmail.com>
2022-02-17 10:46:53 -0800

    GAS Station List

--- Gas.py
+++ Gas.py
@@ -1,17 +1,54 @@
-import random# Gas Level Functiondef gasLevelGauge():
+import random  # Gas Level Functiondef gasLevelGauge():
+
 
 def gasLevelGauge():
-    gasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank","Full"]
+    gasLevelList = ["Empty", "Low", "Quarter Tank", "Half Tank", "Three Quarter Tank", "Full"]
+
     currentGasLevel = random.choice(gasLevelList)
+
     # print(currentGasLevel)
+
     return currentGasLevel
 
-# Create If-ELIF-ELSE statements using comparative operators to display gas level messages
-def gasLevelAlert():
-    if gasLevelGauge() == "Empty":
-        print("***WARNING***\n You have ran out of gas \n Calling Emergency contact")
-    elif gasLevelGauge() == "Low":
-        print("      ***WARNING***\n You are almost out of gasoline\nThe closest gasoline station is ")
 
-#gasLevelGauge()
+# Varible calls the value of gasLevelGauge function
+
+gasLevelIndicator = gasLevelGauge
+
+
+# Create If-ELIF-ELSE statements using the comparative operators == Equal to in order to display gas level messages
+
+def gasLevelAlert():
+    gasStations = ["Shell","BP","Citgo","Circle K","Mobil","Speedway","Marathon","Love's","Meijer","Costco","Sunoco"]
+
+    if gasLevelIndicator == "Empty":
+
+        print("***WARNING YOU ARE ON EMPTY***\n You have ran out of gas \n Calling Emergency contact")
+
+    elif gasLevelIndicator == "Low":
+
+        print("      ***WARNING***\n You are almost out of gasoline\nThe closest gasoline station is " + random.choice(gasStations))
+
+    elif gasLevelIndicator == "Quarter Tank":
+
+        print("you are on a QUARTER TANK of gas\n  start planning a visit to the gas station!!! ")
+
+    elif gasLevelIndicator == "Half Tank":
+
+        print("you are on a HALF TANK of gas\n You have 125 more miles to empty  ")
+
+    elif gasLevelIndicator == "Three Quarter Tank":
+
+        print("you are on a THREE QUARTERS of gas\n You have 205 more miles to empty  ")
+
+    else:
+
+        print("you have a FULL TANK of gas, congratulations\n You have 310 more miles to empty  ")
+
+    # Call Functions Here
+
+
+# gasLevelGauge()
+
 gasLevelAlert()
+
