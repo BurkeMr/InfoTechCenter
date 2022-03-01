da9c11b3eabdf9393495a0c615aa74f9fe889e45
AUSTIN <burkesean902@gmail.com>
2022-03-01 11:22:37 -0800

    Megerged Weather and BetaTestDev

--- MergedBranches.py
+++ MergedBranches.py
@@ -72,3 +72,37 @@ def gasLevelAlert():
 
 gasLevelAlert()
 
+
+import random
+
+# Weather Branch
+
+def weather():
+    weatherForecast = ["Icy","Snowy","Raining","Windy","Foggy","Sunny"]
+    weatherConditions = random.choice(weatherForecast)
+    return weatherConditions
+
+# Calling weather Function to determine weather
+weatherAlert = weather()
+
+def vehicleResponseSystem():
+    if weatherAlert == "Icy":
+        print("\nVRS has changed your alarm 30 minutes earlier based on the NWS forcast",weatherAlert,":(")
+        print("VRS will only allow your car to go 30MPH")
+    elif weatherAlert == "Snowy":
+        print("\nVRS has changed your alarm 15 minutes earlier based on the NWS forcast",weatherAlert,":(")
+        print("VRS will only allow your car to go 50MPH")
+    elif weatherAlert == "Raining":
+        print("\nVRS has changed your alarm 5 minutes earlier based on the NWS forcast", weatherAlert, ":(")
+        print("VRS will only allow your car to go 60MPH")
+    elif weatherAlert == "Windy":
+        print("\nVRS has changed your alarm 5 minutes earlier based on the NWS forcast", weatherAlert, ":(")
+        print("VRS will only allow your car to go 70MPH")
+    elif weatherAlert == "Foggy":
+        print("\nVRS has changed your alarm 5 minutes earlier based on the NWS forcast", weatherAlert, ":(")
+        print("VRS will only allow your car to go 60MPH")
+    else:
+        print("\nWeather is ",weatherAlert,"lets GOOOOOOOOOOO")
+        print("VRS will only allow your car to go 120MPH")
+vehicleResponseSystem()
+
